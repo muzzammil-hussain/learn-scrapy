87ecdd3902dc3ce6667e9b78b43a16219b64455c
Muzzammil Hussain <muzzammilz@gmail.com>
2018-04-24 11:35:50 +0500

    More refined settings

--- learn_scrapy/settings.py
+++ learn_scrapy/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'learn_scrapy.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Study Purpose/No commercial use. Concerns at muzzammilz@gmail.com"
+USER_AGENT = "Study Purpose/No commercial use. Abuse concerns at muzzammilz@gmail.com"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -72,14 +72,14 @@ ROBOTSTXT_OBEY = True
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 0
+AUTOTHROTTLE_START_DELAY = 0.8
 # The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 20
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = True
+#AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
