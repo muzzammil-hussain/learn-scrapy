ccb15e143edb5caea1f39f2a10475182036e9a43
Muzzammil Hussain <muzzammilz@gmail.com>
2018-04-25 12:52:41 +0500

    followed nav links

--- learn_scrapy/spiders/bbcnews.py
+++ learn_scrapy/spiders/bbcnews.py
@@ -1,11 +1,20 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
 class BBCNewsSpider(scrapy.Spider):
     name = "bbcnews"
+    root = "http://www.bbc.com"
+    allowed_domains = ["www.bbc.com"]
     start_urls = [
         "http://www.bbc.com/news",
     ]
 
     def parse(self, response):
-        print(response.css("title::text").extract_first())
+        nav_links = response.xpath("//div[contains(@class, 'nw-o-news-wide-navigation')]//a//@href").extract()
+        for link in nav_links:
+            yield scrapy.Request("{}{}".format(self.root, link), callback=self.parse_category)
+
+    def parse_category(self, response):
+        print(response.xpath('//title/text()').extract_first())
