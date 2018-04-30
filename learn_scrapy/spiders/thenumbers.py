8fab9f73d47ce73e590571751e5d0cd5d3993c8a
Muzzammil Hussain <muzzammilz@gmail.com>
2018-04-30 11:01:32 +0500

    The-numbers spider added

---
+++ learn_scrapy/spiders/thenumbers.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class BBCNewsSpider(CrawlSpider):
+    name = "thenumbers"
+    root = "https://www.the-nuumbers.com"
+    allowed_domains = ["www.the-numbers.com"]
+    start_urls = [
+        "https://www.the-numbers.com",
+    ]
+
+    rules = (
+        Rule(LinkExtractor(allow=(), restrict_xpaths=("",)),
+             callback="parse", follow=True),
+    )
+
+    def parse(self, response):
+        pass
