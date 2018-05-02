af2cea17e9ff52fc62c957f95b1283b2a871398c
Muzzammil Hussain <muzzammilz@gmail.com>
2018-05-02 11:21:54 +0500

    disabled rules object as its not required here.

--- learn_scrapy/spiders/thenumbers.py
+++ learn_scrapy/spiders/thenumbers.py
@@ -9,13 +9,13 @@ class BBCNewsSpider(CrawlSpider):
     allowed_domains = ["www.the-numbers.com"]
     #rotate_user_agent = True
     start_urls = [
-        "https://www.the-numbers.com",
+        "https://www.the-numbers.com/daily-box-office-chart",
     ]
 
-    rules = (
-        Rule(LinkExtractor(allow=(), restrict_xpaths=("",)),
-             callback="parse", follow=True),
-    )
+    # rules = (
+    #     Rule(LinkExtractor(allow=(), restrict_xpaths=("",)),
+    #          callback="parse_list", follow=True),
+    # )
 
     def parse(self, response):
-        print(response)
+        print(response.xpath(".//title/text()").extract_first())
