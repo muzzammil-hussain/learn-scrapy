77339ba51130d35d086bb163754f8344c913a719
Muzzammil Hussain <muzzammilz@gmail.com>
2018-05-07 14:43:12 +0500

    working spider with products list

--- lg_data/lg_data/spiders/fishry.py
+++ lg_data/lg_data/spiders/fishry.py
@@ -4,7 +4,7 @@ from scrapy.spiders import CrawlSpider
 
 
 class FishrySpider(CrawlSpider):
-    name = "sokamal"
+    name = "fishry"
     api_endpoint = "https://fishry-api-live.azurewebsites.net/collection_request"
     collections_endpoint = "https://fishry.azure-mobile.net/tables/collection?$filter=((collectionVisibility eq true) and (storeID eq '{}'))&$top=1000"
     store_id = "480EFD74-078D-4CF2-AC68-270940ED408F"
@@ -15,10 +15,10 @@ class FishrySpider(CrawlSpider):
             "id": "480EFD74-078D-4CF2-AC68-270940ED408F",
             "zumo_id": "egepBriQNqIKWucZFzqpQOMwdDmzfs16",
             "name": "sokamal",
-            "active": True
+            "active": True,
+            "ignore": ["frontpage", "duvet-set", "sheet-set", "media-gallery", "summer-18-catalog" ]
         }
     ]
-
 
     def start_requests(self):
         for store in self.stores:
@@ -26,28 +26,34 @@ class FishrySpider(CrawlSpider):
                 self.collections_endpoint.format(store["id"]),
                 headers = {
                     "X-ZUMO-APPLICATION": store["zumo_id"]
+                },
+                meta={
+                    "ignore": store["ignore"]
                 }
             )
 
-    # def start_requests(self):
-    #     for item in self.collections:
-    #         yield FormRequest(
-    #             self.endpoint,
-    #             formdata={
-    #                 "storeID": self.store_id,
-    #                 "take": "999",
-    #                 "skip": "0",
-    #                 "collection_inclusion": "true",
-    #                 "order_by": "__createdat",
-    #                 "order_by_seq": "desc",
-    #                 "varients_inclusion": "true",
-    #                 "status": "true",
-    #                 "collection_id[]": item,
-    #                 "varients": "[]"
-    #             })
-
     def parse(self, response):
-        print("**********************")
+        json_response = json.loads(response.body_as_unicode())
+        for item in json_response:
+            if item["collectionUrl"] not in response.meta["ignore"]:
+                yield FormRequest(
+                    self.api_endpoint,
+                    formdata={
+                        "storeID": self.store_id,
+                        "take": "999",
+                        "skip": "0",
+                        "collection_inclusion": "true",
+                        "order_by": "__createdat",
+                        "order_by_seq": "desc",
+                        "varients_inclusion": "true",
+                        "status": "true",
+                        "collection_id[]": item["id"],
+                        "varients": "[]"
+                    },
+                    callback=self.parse_category
+                )
+
+    def parse_category(self, response):
         json_response = json.loads(response.body_as_unicode())
         for item in json_response:
             print(item["id"])
