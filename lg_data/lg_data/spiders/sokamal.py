0c62ce95c6e199177680d569408e16d6e12021c6
Muzzammil Hussain <muzzammilz@gmail.com>
2018-05-07 14:03:53 +0500

    working collections call

--- lg_data/lg_data/spiders/sokamal.py
+++ lg_data/lg_data/spiders/sokamal.py
@@ -1,30 +1,31 @@
 import json
-from scrapy import FormRequest
+from scrapy import FormRequest, Request
 from scrapy.spiders import CrawlSpider
 
 
 class SoKamalSpider(CrawlSpider):
     name = "sokamal"
-    endpoint = "https://fishry-api-live.azurewebsites.net/collection_request"
+    api_endpoint = "https://fishry-api-live.azurewebsites.net/collection_request"
+    collections_endpoint = "https://fishry.azure-mobile.net/tables/collection?$filter=((collectionVisibility eq true) and (storeID eq '{}'))&$top=1000"
     store_id = "480EFD74-078D-4CF2-AC68-270940ED408F"
     rotate_user_agent = True
+    fishry_stores = [
+        {
+            "id": "480EFD74-078D-4CF2-AC68-270940ED408F",
+            "zumo_id": "egepBriQNqIKWucZFzqpQOMwdDmzfs16",
+            "name": "sokamal",
+            "active": True
+        }
+    ]
 
     def start_requests(self):
-        for item in self.collections:
-            yield FormRequest(
-                self.endpoint,
-                formdata={
-                    "storeID": self.store_id,
-                    "take": "999",
-                    "skip": "0",
-                    "collection_inclusion": "true",
-                    "order_by": "__createdat",
-                    "order_by_seq": "desc",
-                    "varients_inclusion": "true",
-                    "status": "true",
-                    "collection_id[]": item,
-                    "varients": "[]"
-                })
+        for store in self.fishry_stores:
+            yield Request(
+                self.collections_endpoint.format(store["id"]),
+                headers = {
+                    "X-ZUMO-APPLICATION": store["zumo_id"]
+                }
+            )
 
     # def start_requests(self):
     #     for item in self.collections:
