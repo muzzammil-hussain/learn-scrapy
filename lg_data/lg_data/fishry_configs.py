5e53d2f1c0450a778039a560a9370a8bef6d9999
Muzzammil Hussain <muzzammilz@gmail.com>
2018-05-15 15:37:02 +0500

    id added with store

--- lg_data/lg_data/fishry_configs.py
+++ lg_data/lg_data/fishry_configs.py
@@ -1,6 +1,7 @@
 stores = {
     "480EFD74-078D-4CF2-AC68-270940ED408F": {
-        "name": "sokamal",
+        "id": 1,
+        "name": "So Kamal",
         "active": True,
         "ignore": ["frontpage", "duvet-set", "sheet-set", "media-gallery", "summer-18-catalog"]
     }
