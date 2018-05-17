9df33d321b83ad7de7a429c9e9697ff816646caf
Muzzammil Hussain <muzzammilz@gmail.com>
2018-05-17 17:21:58 +0500

    new store added

--- lg_data/lg_data/fishry_configs.py
+++ lg_data/lg_data/fishry_configs.py
@@ -4,5 +4,19 @@ stores = {
         "name": "So Kamal",
         "active": True,
         "ignore": ["frontpage", "duvet-set", "sheet-set", "media-gallery", "summer-18-catalog"]
+    },
+    "99C2F6A4-008E-47E7-A31A-459957905A0D": {
+        "id": 1,
+        "name": "Nishat Linen",
+        "active": True,
+        "ignore": [
+            "naqsh-summer-2018", "kids-eid-collection-2018", "kids-summer-collection-2018", "kids-winter-collection-2017",
+            "kids-summer-17", "lowers", "kids-hair-accessories", "kids-necklaces", "handbags", "clutches", "backpacks",
+            "wallets", "vanity-bags", "bracelets-bangles", "earrings", "rings", "necklaces", "sunglasses", "sunglasses-box",
+            "wraps", "footwear", "camisole", "dyed-bed-sheets", "printed-bed-sheets", "bridal-sets", "bed-in-a-bag",
+            "kids-bedding", "duvet-covers", "bedspreads", "comforters", "cushions", "fillings", "curtains", "towels",
+            "table-runners", "tea-cozy", "kitchen-accessories"
+        ]
     }
+
 }
