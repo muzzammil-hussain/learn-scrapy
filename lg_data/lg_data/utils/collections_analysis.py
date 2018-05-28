813e1c53a044110b6a1a07d4c412b708d131f3c9
Muzzammil Hussain <muzzammilz@gmail.com>
2018-05-28 12:44:06 +0500

    reading results.json file

---
+++ lg_data/lg_data/utils/collections_analysis.py
@@ -0,0 +1,18 @@
+import json
+from os.path import join, dirname
+
+# load data file
+
+with open(join(dirname(__file__), "../../result.json")) as fp:
+    results = json.loads(str(fp.read()))
+
+print(results)
+
+# if wid_route:
+#     LOGGER.info('Writing route map: {}'.format(filename))
+#     separators = (',', ': ')
+#     ordered_map = save_map(wid_route)
+#     open(filename, 'w').write(
+#         json.dumps(ordered_map, indent=2, separators=separators)
+#     )
+#
