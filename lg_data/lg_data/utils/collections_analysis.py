7a1464a1d4a963ad252ada4f3925346551a32429
Muzzammil Hussain <muzzammilz@gmail.com>
2018-05-28 12:57:59 +0500

    collections calculated

--- lg_data/lg_data/utils/collections_analysis.py
+++ lg_data/lg_data/utils/collections_analysis.py
@@ -6,7 +6,13 @@ from os.path import join, dirname
 with open(join(dirname(__file__), "../../result.json")) as fp:
     results = json.loads(str(fp.read()))
 
-print(results)
+final_collections = []
+for result in results:
+    for collection in result["collections"]:
+      if collection not in final_collections:
+          final_collections.append(collection)
+
+print(set(final_collections))
 
 # if wid_route:
 #     LOGGER.info('Writing route map: {}'.format(filename))
